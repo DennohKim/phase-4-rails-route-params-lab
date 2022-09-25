5b68df271e83dcc7fdc810d0cfbaae582ecdd2b4
Dennis Kimathi <kimathidennis38@gmail.com>
2022-09-25 13:55:10 +0300

    commit tree 0d89cafb9a807b5260c096548fe7833cabeea98d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,24 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    q_first_name = params[:first_name]
+    q_last_name = params[:last_name]
+
+    students = if first_name 
+      Student.find_by(first_name: q_first_name)
+    elsif last_name 
+      Student.find_by(last_name: q_last_name)
+    else    
+      Student.all
+    end
+
     render json: students
   end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
 
 end
